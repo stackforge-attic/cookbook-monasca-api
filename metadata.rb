0c645f07d3d422166742b42d382fabc20266b912
Tim Kuhlman <tim.kuhlman@hp.com>
2014-05-27 11:00:47 -0600

    Fixed zookeeper url

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "hpcs-mon-som@hp.com"
 license          "All rights reserved"
 description      "Installs/Configures mon_api"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
